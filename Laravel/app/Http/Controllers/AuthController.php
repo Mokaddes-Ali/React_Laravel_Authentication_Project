0dfccf3c5d40eb7c22518046ca1153272a73bd25
mokadde <mokaddes.ru2000@gmail.com>
2024-11-04 12:45:49 +0600

    commit

--- Laravel/app/Http/Controllers/AuthController.php
+++ Laravel/app/Http/Controllers/AuthController.php
@@ -3,10 +3,13 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Illuminate\Auth\Events\PasswordReset;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
-use Dotenv\Exception\ValidationException;
+use Illuminate\Support\Facades\Password;
+use Illuminate\Validation\ValidationException;
 
 class AuthController extends Controller
 {
@@ -15,9 +18,11 @@ class AuthController extends Controller
         $user = User::create($request->validate([
             'first_name' => 'required|string',
             'last_name' => 'string',
-            'email' => 'required|email|unique:users,email',
-            'password' => 'required|string|min:8',
+            'email' => 'required|email',
+            'password' => 'required|min:8',
         ]));
+
+        $user->sendEmailVerificationNotification();
 
         return response()->json([
             'message' => 'User created successfully',
@@ -33,28 +38,138 @@ class AuthController extends Controller
             'remember' => 'boolean',
         ]);
 
-        $credentials = (
+        $credentials = [
             'email' => $fields['email'],
             'password' => $fields['password'],
-        );
+        ];
 
         if (!Auth::attempt($credentials, $fields['remember'])) {
             throw ValidationException::withMessages([
-                'email' => 'Invalid credentials',
+                'email' => ['The provided cretials are incorrect'],
             ]);
+
+            session()->regenerate();
 
 
             return response()->json([
-                'message' => 'Invalid credentials',
-            ], 401);
+                'message' => 'Succe3ssfully Logned in',
+                'user' => Auth::user(),
+            ]);
         }
-
-
-
-
-
-        return response()->json([
-            'message' => 'User logged in successfully',
-        ]);
     }
+
+ public function logout(){
+                Auth::guard('web')->logout();
+                return response()->json([
+                    'message' => 'User logged out successfully',
+                ]);
+            }
+
+
+    // public function emailVerify($user_id,Request $request)
+    // {
+    //     if(!$request->hasValidSignature()){
+    //         return response()->json([
+    //             'message' => 'Invalid/Expired url provided',
+    //         ], 400);
+    //     }
+
+    //     $user = User::findOrFail($user_id);
+
+    //     if (!$user) {
+    //         return response()->json([
+    //             'message' => 'User not found.',
+    //         ], 400);
+    //     }
+
+    //     if ($user->hasVerifiedEmail()) {
+    //         $user->markEmailAsVerified();
+    //         return response()->json([
+    //             'message' => 'Email already verified.',
+    //             'user' => $user,
+    //         ]);
+    //     }
+
+    //     return response()->json([
+    //         'message' => 'Email addres already verified.',
+    //     ], 400);
+    // }
+
+    // public function resendEmailVerification(Request $request)
+    // {
+    //     $user_id = $request->input('user_id');
+
+    //     $user = User::findOrFail($user_id);
+
+    //     if (!$user) {
+    //         return response()->json([
+    //             'message' => 'User not found.',
+    //         ], 400);
+    //     }
+
+
+    //     if ($user->hasVerifiedEmail()) {
+    //         return response()->json([
+    //             'message' => 'Email already verified.',
+    //         ], 400);
+    //     }
+
+    //     $user->sendEmailVerificationNotification();
+
+    //     return response()->json([
+    //         'message' => 'Email verification link sent on your email id.',
+    //     ]);
+    // }
+
+    // public function forgotPassword(Request $request)
+    // {
+    //     $request->validate([
+    //         'email' => 'required|email',
+    //     ]);
+
+    //     $status = Password::sendResetLink(
+    //         $request->only('email')
+    //     );
+
+    //     return $status === Password::RESET_LINK_SENT
+    //                 ? response()->json([
+    //                     'message' => trans($status)
+    //                     ])
+    //                 : response()->json([
+    //                     'message' => trans($status)
+    //                 ], 400);
+    // }
+
+    // public function resetPassword(Request $request)
+    // {
+    //     $request->validate([
+    //         'token' => 'required',
+    //         'email' => 'required|email',
+    //         'password' => 'required|string|min:8|confirmed',
+    //     ]);
+
+    //     $status = Password::reset(
+    //         $request->only('email', 'password', 'password_confirmation', 'token'),
+    //         function (User $user, string $password) {
+    //             $user->forceFill([
+    //                 'password' => Hash::make($password)
+    //             ])->setRememberToken(Str::random(60));
+
+    //             $user->save();
+
+    //             event(new PasswordReset($user));
+    //         }
+    //     );
+
+
+    //     return $status === Password::PASSWORD_RESET
+    //                 ? response()->json([
+    //                 'message' => trans($status),
+    //                 ])
+    //                 : response()->json([
+    //                     'message' =>trans($status),
+    //                 ], 400);
+
+    // }
+
 }
